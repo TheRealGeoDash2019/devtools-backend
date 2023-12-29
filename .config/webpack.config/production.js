36e69aab0d24bb55353322ff24ca0c7d9aa2306f
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2023-12-29 23:10:05 +0000

    fix webpack configs

--- .config/webpack.config/production.js
+++ .config/webpack.config/production.js
@@ -1,5 +1,7 @@
 import webpack from 'webpack'
 import merge from 'webpack-merge'
+import DuplicatePackageCheckerWebpackPlugin from 'duplicate-package-checker-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
 
 import common from './common'
 
@@ -8,20 +10,23 @@ const config = {
     //debug: false,
     profile: false,
     devtool: 'source-map',
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(true),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                drop_console: true,
-                drop_debugger: true
-            },
-            output: {
-                comments: false
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true
+                },
+                output: {
+                    comments: false
+                }
             }
-        })
+        })],
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new DuplicatePackageCheckerWebpackPlugin(),
     ],
     module: {
         loaders: []
